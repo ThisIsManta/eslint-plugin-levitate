d28f6f4eb3e3165dc153b72c34eae9c11534816a
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2018-04-02 22:14:17 +0700

    Added use-promise-all-with-static-array

---
+++ edge/use-promise-all-with-static-array.js
@@ -0,0 +1,112 @@
+const _ = require('lodash')
+
+const REQUIRE_BLUEBIRD = {
+	id: {
+		type: 'Identifier',
+	},
+	init: {
+		type: 'CallExpression',
+		callee: {
+			type: 'Identifier',
+			name: 'require',
+		},
+		arguments: [
+			{
+				type: 'Literal',
+				value: 'bluebird',
+			}
+		]
+	}
+}
+
+const METHOD_ALL = {
+	type: 'Identifier',
+	name: 'all',
+}
+
+module.exports = {
+	meta: {
+		docs: {
+			description: 'enforce passing a static array to Promise.all()',
+			category: 'Best Practices',
+		},
+	},
+	create: function (context) {
+		let bluebirdIdentifier
+		return {
+			VariableDeclarator: function (workNode) {
+				if (_.isMatch(workNode, REQUIRE_BLUEBIRD)) {
+					bluebirdIdentifier = workNode.id.name
+				}
+			},
+			CallExpression: function (workNode) {
+				if (_.isMatch(workNode.callee.property, METHOD_ALL) === false) {
+					return null
+				}
+
+				const identifier = _.get(workNode, 'callee.object.name')
+				if (identifier === undefined) {
+					return null
+				}
+
+				if (identifier !== 'Promise' && identifier !== bluebirdIdentifier) {
+					return null
+				}
+
+				if (workNode.arguments.length === 0) {
+					return null
+				}
+
+				if (workNode.arguments[0].type !== 'ArrayExpression' || workNode.arguments[0].elements.some(node => node.type === 'SpreadElement')) {
+					return context.report({
+						node: workNode.arguments[0],
+						message: `Expected ${identifier}.all() to have a parameter of a static array.`,
+					})
+				}
+			}
+		}
+	},
+	test: {
+		valid: [
+			'Promise.all()',
+			'Promise.all([])',
+			'Promise.all([1, 2, 3])',
+			{
+				code: `
+					const Bluebird = require('bluebird')
+					Bluebird.all([1, 2, 3])
+				`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+				errors: [{ message: 'Expected Bluebird.all() to have a parameter of a static array.', }]
+			},
+		],
+		invalid: [
+			{
+				code: 'Promise.all([1, 2, 3, ...x])',
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+				errors: [{ message: 'Expected Promise.all() to have a parameter of a static array.', }]
+			},
+			{
+				code: 'Promise.all(x)',
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+				errors: [{ message: 'Expected Promise.all() to have a parameter of a static array.', }]
+			},
+			{
+				code: `
+					const Bluebird = require('bluebird')
+					Bluebird.all([1, 2, 3, ...x])
+				`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+				errors: [{ message: 'Expected Bluebird.all() to have a parameter of a static array.', }]
+			},
+			{
+				code: `
+					const Bluebird = require('bluebird')
+					Bluebird.all(x)
+				`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+				errors: [{ message: 'Expected Bluebird.all() to have a parameter of a static array.', }]
+			},
+		]
+	}
+}
--- rules.js
+++ rules.js
@@ -5,6 +5,7 @@ module.exports = [
 	'sort-imports',
 	'use-catch-parameter-after-predefined-name',
 	'use-import-name-after-predefined-list',
+	'use-promise-all-with-static-array',
 	'use-require-name-after-file-path',
 	'use-require-name-after-predefined-list',
 ]
