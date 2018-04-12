b43eedb1b20ccc13c0b210b52b7734a4e3ea7b78
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2018-04-13 01:13:52 +0700

    Amended use-import-name-after-predefined-list

--- edge/use-import-name-after-predefined-list.js
+++ edge/use-import-name-after-predefined-list.js
@@ -11,13 +11,18 @@ module.exports = {
 		schema: [
 			{ type: 'object' }
 		],
-		fixable: 'code',
 	},
 	create: function (context) {
-		let nameDict = {}
+		const ruleList = []
 		if (context.options.length > 0 && _.isObject(context.options[0])) {
-			_.forEach(context.options[0], function (path, name) {
-				nameDict[path] = name
+			_.forEach(context.options[0], function (importPath, variableName) {
+				let importPathMatcher
+				if (importPath.startsWith('/')) {
+					importPathMatcher = new RegExp(importPath.substring(1, importPath.lastIndexOf('/'), importPath.substring(importPath.lastIndexOf('/') + 1)))
+				} else {
+					importPathMatcher = new RegExp('^' + _.escapeRegExp(importPath) + '$')
+				}
+				ruleList.push([importPathMatcher, variableName])
 			})
 		}
 
@@ -27,26 +32,26 @@ module.exports = {
 					return null
 				}
 
-				const path = root.source.value
-				if (nameDict[path] === undefined) {
-					return null
-				}
+				const workPath = root.source.value
+				const workNode = root.specifiers.find(node => node.type === 'ImportNamespaceSpecifier' || node.type === 'ImportDefaultSpecifier')
 
-				if (root.specifiers.length > 1 || root.specifiers.some(node => node.type === 'ImportDefaultSpecifier') === false) {
-					return context.report({
-						node: root,
-						message: `Expected to import "${nameDict[path]}".`,
-						fix: fixer => fixer.replaceText(root, `import ${nameDict[path]} from '${path}'`)
-					})
-				}
+				for (const rule of ruleList) {
+					const [importPathMatcher, variableName] = rule
+					if (importPathMatcher.test(workPath)) {
+						if (workNode === undefined) {
+							return context.report({
+								node: root,
+								message: `Expected to import "${variableName}" or "* as ${variableName}".`,
+							})
+						}
 
-				const name = root.specifiers.find(node => node.type === 'ImportDefaultSpecifier').local.name
-				if (nameDict[path] !== name) {
-					return context.report({
-						node: name ? root.specifiers[0].local : root,
-						message: `Expected "${name}" to be "${nameDict[path]}".`,
-						fix: fixer => fixer.replaceText(root, `import ${nameDict[path]} from '${path}'`)
-					})
+						if (workNode.local.name !== variableName) {
+							return context.report({
+								node: workNode.local,
+								message: `Expected "${workNode.local.name}" to be "${variableName}".`,
+							})
+						}
+					}
 				}
 			}
 		}
@@ -68,6 +73,26 @@ module.exports = {
 				options: [{ AAA: 'aaa' }],
 				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
 			},
+			{
+				code: `import AAA from './aaa'`,
+				options: [{ 'AAA': '//aaa$/' }],
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `import AAA from './aaa'`,
+				options: [{ 'AAA': '//aaa$/' }],
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `import AAA from 'aaa123'`,
+				options: [{ 'AAA$1': '/aaa(\d+)/' }],
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `import XXX from 'AAA'`,
+				options: [{ 'XXX': '/aaa/i' }],
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
 		],
 		invalid: [
 			{
@@ -81,7 +106,7 @@ module.exports = {
 				options: [{ AAA: 'aaa' }],
 				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
 				errors: [
-					{ message: 'Expected to import "AAA".' }
+					{ message: 'Expected to import "AAA" or "* as AAA".' }
 				],
 			},
 		]
