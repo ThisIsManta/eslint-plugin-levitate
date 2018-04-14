a40948dc00ae07c6b36f8ee267abc3762bc36be3
Anantachai Saothong (Manta) <thisismanta@outlook.com>
2018-04-14 18:00:13 +0700

    Fixed use-import-name-after-file-or-directory-name

--- edge/use-import-name-after-file-or-directory-name.js
+++ edge/use-import-name-after-file-or-directory-name.js
@@ -6,7 +6,7 @@ const _ = require('lodash')
 module.exports = {
 	meta: {
 		docs: {
-			description: 'enforce writing variable name after the user-defined list',
+			description: 'enforce writing variable name after file or directory name',
 			category: 'Variables',
 		},
 	},
@@ -17,15 +17,18 @@ module.exports = {
 					return null
 				}
 
+				const workPath = root.source.value
+				if (workPath.startsWith('.') === false) {
+					return null
+				}
+
 				const workNode = root.specifiers.find(node => node.type === 'ImportNamespaceSpecifier' || node.type === 'ImportDefaultSpecifier')
 				if (workNode === undefined) {
 					return null
 				}
 
-				const workPath = root.source.value
 				const fileName = pt.basename(workPath, pt.extname(workPath))
 				const dirxName = pt.dirname(workPath)
-
 				const camelName = _.camelCase(fileName === 'index' ? dirxName : fileName)
 				const pascalName = camelName.charAt(0).toUpperCase() + camelName.substring(1)
 				if (workNode.local.name !== pascalName) {
@@ -40,23 +43,19 @@ module.exports = {
 	test: {
 		valid: [
 			{
-				code: `import { a } from 'aaa'`,
+				code: `import { a } from './aaa'`,
 				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
 			},
 			{
-				code: `import * as Aaa from 'aaa'`,
-				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
-			},
-			{
-				code: `import Aaa from 'aaa'`,
-				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
-			},
-			{
-				code: `import Aaa from 'aaa.js'`,
+				code: `import * as Aaa from './aaa'`,
 				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
 			},
 			{
 				code: `import Aaa from './aaa'`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `import Aaa from './aaa.js'`,
 				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
 			},
 			{
@@ -70,12 +69,12 @@ module.exports = {
 		],
 		invalid: [
 			{
-				code: `import XXX from 'aaa'`,
+				code: `import XXX from './aaa'`,
 				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
 				errors: [{ message: 'Expected "XXX" to be "Aaa".' }],
 			},
 			{
-				code: `import * as XXX from 'aaa'`,
+				code: `import * as XXX from './aaa'`,
 				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
 				errors: [
 					{ message: 'Expected "XXX" to be "Aaa".' }
