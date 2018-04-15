bde9658c616edf995efdd9d8d9e7eef1e1566c57
Anantachai Saothong (Manta) <thisismanta@outlook.com>
2018-04-15 13:40:23 +0700

    Fixed ambiguous file and directory names

--- edge/use-import-name-after-file-or-directory-name.js
+++ edge/use-import-name-after-file-or-directory-name.js
@@ -7,98 +7,94 @@ const { getImportFullPath } = require('./use-import-path-from-the-closest-index'
 const ALPHANUMERIC = /[A-Za-z0-9]/
 
 module.exports = {
-  meta: {
-    docs: {
-      description: 'enforce writing variable name after file or directory name',
-      category: 'Variables',
-    },
-  },
-  create: function (context) {
-    return {
-      ImportDeclaration: function (root) {
-        if (!root.specifiers || root.specifiers.length === 0) {
-          return null
-        }
+	meta: {
+		docs: {
+			description: 'enforce writing variable name after file or directory name',
+			category: 'Variables',
+		},
+	},
+	create: function (context) {
+		return {
+			ImportDeclaration: function (root) {
+				if (!root.specifiers || root.specifiers.length === 0) {
+					return null
+				}
 
-        const workPath = root.source.value
-        if (workPath.startsWith('.') === false) {
-          return null
-        }
+				const workPath = root.source.value
+				if (workPath.startsWith('.') === false) {
+					return null
+				}
 
-        const workNode = root.specifiers.find(node => node.type === 'ImportNamespaceSpecifier' || node.type === 'ImportDefaultSpecifier')
-        if (workNode === undefined) {
-          return null
-        }
+				const workNode = root.specifiers.find(node => node.type === 'ImportNamespaceSpecifier' || node.type === 'ImportDefaultSpecifier')
+				if (workNode === undefined) {
+					return null
+				}
 
-        const fullPath = getImportFullPath(context.getFilename(), workPath) || workPath
-        const fileNameWithoutExtension = pt.basename(fullPath, pt.extname(fullPath))
-        const directoryName = pt.basename(pt.dirname(fullPath))
-        const expectedName = fileNameWithoutExtension === 'index'
-          ? _.get(directoryName.match(ALPHANUMERIC), '0', '').toUpperCase() + _.camelCase(directoryName).substring(1)
-          : _.get(fileNameWithoutExtension.match(ALPHANUMERIC), '0', '') + _.camelCase(fileNameWithoutExtension).substring(1)
+				const fullPath = getImportFullPath(context.getFilename(), workPath) || workPath
+				const fileNameWithoutExtension = pt.basename(fullPath, pt.extname(fullPath))
+				const directoryName = pt.basename(pt.dirname(fullPath))
+				const expectedName = fileNameWithoutExtension === 'index'
+					? _.get(directoryName.match(ALPHANUMERIC), '0', '').toUpperCase() + _.camelCase(directoryName).substring(1)
+					: _.get(fileNameWithoutExtension.match(ALPHANUMERIC), '0', '') + _.camelCase(fileNameWithoutExtension).substring(1)
 
-        // Check for another duplicate identifier
-        const otherImports = _.last(context.getAncestors()).body.filter(node => node.type === 'ImportDeclaration' && node !== root)
-        if (otherImports.some(node => node.specifiers && node.specifiers.some(spec => spec.local.name === expectedName))) {
-          return null
-        }
+				// Check for another duplicate identifier
+				const otherImports = _.last(context.getAncestors()).body.filter(node => node.type === 'ImportDeclaration' && node !== root)
+				if (otherImports.some(node => node.specifiers && node.specifiers.some(spec => spec.local.name === expectedName))) {
+					return null
+				}
 
-        if (workNode.local.name !== expectedName) {
-          return context.report({
-            node: workNode.local,
-            message: `Expected "${workNode.local.name}" to be "${expectedName}".`,
-          })
-        }
-      }
-    }
-  },
-  test: {
-    valid: [
-      {
-        code: `import { a } from './aaa'`,
-        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
-      },
-      {
-        code: `import * as aaa from './aaa'`,
-        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
-      },
-      {
-        code: `import aaa from './aaa'`,
-        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
-      },
-      {
-        code: `import aaa from './aaa.js'`,
-        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
-      },
-      {
-        code: `import aaa from '../xxx-yyy/aaa'`,
-        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
-      },
-      {
-        code: `import XxxYyy from '../xxx-yyy/index'`,
-        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
-      },
-    ],
-    invalid: [
-      {
-        code: `import XXX from './aaa'`,
-        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
-        errors: [ { message: 'Expected "XXX" to be "aaa".' } ],
-      },
-      {
-        code: `import * as XXX from './aaa'`,
-        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
-        errors: [
-          { message: 'Expected "XXX" to be "aaa".' }
-        ],
-      },
-      {
-        code: `import * as XXX from '../xxx-yyy/index'`,
-        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
-        errors: [
-          { message: 'Expected "XXX" to be "XxxYyy".' }
-        ],
-      },
-    ]
-  }
+				if (workNode.local.name !== expectedName) {
+					return context.report({
+						node: workNode.local,
+						message: `Expected "${workNode.local.name}" to be "${expectedName}".`,
+					})
+				}
+			}
+		}
+	},
+	test: {
+		valid: [
+			{
+				code: `import { a } from './aaa'`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `import * as aaa from './aaa'`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `import aaa from './aaa'`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `import aaa from './aaa.js'`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `import aaa from '../xxx-yyy/aaa'`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `import XxxYyy from '../xxx-yyy/index'`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+		],
+		invalid: [
+			{
+				code: `import XXX from './aaa'`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+				errors: [{ message: 'Expected "XXX" to be "aaa".' }],
+			},
+			{
+				code: `import * as XXX from './aaa'`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+				errors: [{ message: 'Expected "XXX" to be "aaa".' }],
+			},
+			{
+				code: `import * as XXX from '../xxx-yyy/index'`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+				errors: [{ message: 'Expected "XXX" to be "XxxYyy".' }],
+			},
+		]
+	}
 }
--- edge/use-import-path-from-the-closest-index.js
+++ edge/use-import-path-from-the-closest-index.js
@@ -5,84 +5,89 @@ const fs = require('fs')
 const pt = require('path')
 
 module.exports = {
-  meta: {
-    docs: {
-      description: 'enforce importing the closest index file',
-      category: 'ECMAScript 6',
-    },
-  },
-  create: function (context) {
-    return {
-      ImportDeclaration: function (root) {
-        const relativePath = root.source.value
-        if (relativePath.startsWith('.') === false) {
-          return null
-        }
+	meta: {
+		docs: {
+			description: 'enforce importing the closest index file',
+			category: 'ECMAScript 6',
+		},
+	},
+	create: function (context) {
+		return {
+			ImportDeclaration: function (root) {
+				const relativePath = root.source.value
+				if (relativePath.startsWith('.') === false) {
+					return null
+				}
 
-        const fullPath = getImportFullPath(context.getFilename(), relativePath)
-        if (fullPath === null) {
-          return null
-        }
+				const fullPath = getImportFullPath(context.getFilename(), relativePath)
+				if (fullPath === null) {
+					return null
+				}
 
-        const supportedExtensions = getSupportedExtensions(fullPath)
+				const supportedExtensions = getSupportedExtensions(fullPath)
 
-        const rootPath = process.cwd()
-        const workPath = fullPath.substring(rootPath.length)
+				const rootPath = process.cwd()
+				const workPath = fullPath.substring(rootPath.length)
 
-        const workPathList = _.compact(workPath.split(/\\|\//))
-        for (let count = 1; count <= workPathList.length; count++) {
-          const testPath = workPathList.slice(0, count).join(pt.sep)
-          for (const extension of supportedExtensions) {
-            const expectedPath = pt.join(rootPath, testPath, 'index' + extension)
-            if (fs.existsSync(expectedPath)) {
-              if (fullPath.startsWith(pt.dirname(expectedPath))) {
-                return null
-              }
+				const workPathList = _.compact(workPath.split(/\\|\//))
+				for (let count = 1; count <= workPathList.length; count++) {
+					const testPath = workPathList.slice(0, count).join(pt.sep)
+					for (const extension of supportedExtensions) {
+						const expectedPath = pt.join(rootPath, testPath, 'index' + extension)
+						if (fs.existsSync(expectedPath)) {
+							if (fullPath.startsWith(pt.dirname(expectedPath))) {
+								return null
+							}
 
-              if (expectedPath !== fullPath) {
-                const unixPath = _.trim(expectedPath.substring(rootPath.length).replace(/\\/, '/'), '/')
-                return context.report({
-                  node: root.source,
-                  message: `Expected to import "${unixPath}".`,
-                })
-              }
+							if (expectedPath !== fullPath) {
+								const unixPath = _.trim(expectedPath.substring(rootPath.length).replace(/\\/, '/'), '/')
+								return context.report({
+									node: root.source,
+									message: `Expected to import "${unixPath}".`,
+								})
+							}
 
-              break
-            }
-          }
-        }
-      }
-    }
-  },
-  getSupportedExtensions,
-  getImportFullPath,
+							break
+						}
+					}
+				}
+			}
+		}
+	},
+	getSupportedExtensions,
+	getImportFullPath,
 }
 
-function getSupportedExtensions (currentFullPath) {
-  return pt.extname(currentFullPath) === '.ts' ? [ '.ts', '.js' ] : [ '.js' ]
+function getSupportedExtensions(currentFullPath) {
+	return pt.extname(currentFullPath) === '.ts' ? ['.ts', '.js'] : ['.js']
 }
 
-function getImportFullPath (currentFullPath, importRelativePath) {
-  const supportedExtensions = getSupportedExtensions(currentFullPath)
+function getImportFullPath(currentFullPath, importRelativePath) {
+	const supportedExtensions = getSupportedExtensions(currentFullPath)
 
-  const fullPath = pt.resolve(pt.dirname(currentFullPath), importRelativePath)
-  if (fs.existsSync(fullPath) && fs.lstatSync(fullPath).isDirectory()) {
-    for (const extension of supportedExtensions) {
-      const actualPath = pt.join(fullPath, 'index' + extension)
-      if (fs.existsSync(actualPath)) {
-        return actualPath
-      }
-    }
+	const fullPath = fp.resolve(fp.dirname(currentFullPath), importRelativePath)
+	if (fp.extname(fullPath) === '') {
+		for (const extension of supportedExtensions) {
+			if (fs.existsSync(fullPath + extension)) {
+				return fullPath + extension
+			}
+		}
+	}
 
-  } else if (pt.extname(fullPath) === '') {
-    for (const extension of supportedExtensions) {
-      if (fs.existsSync(fullPath + extension)) {
-        return fullPath + extension
-      }
-    }
-  }
+	if (fs.existsSync(fullPath)) {
+		if (fs.lstatSync(fullPath).isDirectory()) {
+			for (const extension of supportedExtensions) {
+				const actualPath = fp.join(fullPath, 'index' + extension)
+				if (fs.existsSync(actualPath)) {
+					return actualPath
+				}
+			}
 
-  if (fs.existsSync(fullPath) === false || fullPath === undefined) {
-    return null
-  }
+		} else {
+			return fullPath
+		}
+
+	} else {
+		return null
+	}
 }
