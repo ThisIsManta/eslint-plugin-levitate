c59ab33b268c058164fc734f4e019aa5bba9d8b0
Anantachai Saothong (Manta) <thisismanta@outlook.com>
2018-08-10 17:51:30 +0700

    Fixed typescript-array-type

--- edge/typescript-array-type.js
+++ edge/typescript-array-type.js
@@ -18,10 +18,15 @@ module.exports = {
 		return {
 			TSArrayType: function (root) {
 				if (context.options[0] === 'generic') {
+					const innerType = root.elementType
+					let innerText = sourceCode.getText(innerType).trim()
+					if (innerType.type === 'TSParenthesizedType' && innerText.startsWith('(') && innerText.endsWith(')')) {
+						innerText = innerText.substring(1, innerText.length - 1)
+					}
 					return context.report({
 						node: root,
 						message: `Expected an array type must be written in the square bracket notation.`,
-						fix: fixer => fixer.replaceText(root, `Array<${sourceCode.getText(root.elementType)}>`)
+						fix: fixer => fixer.replaceText(root, `Array<${innerText}>`)
 					})
 				}
 			},
@@ -31,15 +36,19 @@ module.exports = {
 					root.typeName && root.typeName.type === 'Identifier' && root.typeName.name === 'Array' &&
 					root.typeParameters && root.typeParameters.type === 'TSTypeParameterInstantiation' && root.typeParameters.params.length === 1
 				) {
-					const innerTypeNotation = root.typeParameters.params[0]
-					let innerTypeText = sourceCode.getText(innerTypeNotation)
-					if (innerTypeNotation.type === 'TSTypeReference' && innerTypeText.startsWith('(') === false) {
-						innerTypeText = '(' + innerTypeText + ')'
+					const innerType = root.typeParameters.params[0]
+					let innerText = sourceCode.getText(innerType).trim()
+					if (
+						innerType.type === 'TSTypeReference' &&
+						(innerType.typeName.type === 'TSUnionType' || innerType.typeName.type === 'TSIntersectionType')
+					) {
+						innerText = '(' + innerText + ')'
 					}
+
 					return context.report({
 						node: root,
 						message: `Expected an array type must be written in a generic type.`,
-						fix: fixer => fixer.replaceText(root, `${innerTypeText}[]`)
+						fix: fixer => fixer.replaceText(root, `${innerText}[]`)
 					})
 				}
 			},
@@ -87,12 +96,28 @@ module.exports = {
 				output: `type x = Array<any>`,
 			},
 			{
+				code: `type x = (number | string)[]`,
+				options: ['generic'],
+				parser: 'typescript-eslint-parser',
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+				errors: [{ message: 'Expected an array type must be written in the square bracket notation.' }],
+				output: `type x = Array<number | string>`,
+			},
+			{
 				code: `type x = Array<any>`,
 				options: ['bracket'],
 				parser: 'typescript-eslint-parser',
 				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
 				errors: [{ message: 'Expected an array type must be written in a generic type.' }],
 				output: `type x = any[]`,
+			},
+			{
+				code: `type x = Array<IType>`,
+				options: ['bracket'],
+				parser: 'typescript-eslint-parser',
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+				errors: [{ message: 'Expected an array type must be written in a generic type.' }],
+				output: `type x = IType[]`,
 			},
 			{
 				code: `type x = Array<number | string>`,
