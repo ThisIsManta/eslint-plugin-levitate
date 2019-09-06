c795dc6f0a1c1d3077ec5ecdc17e40fe51ceeb2a
Anantachai Saothong (Manta) <thisismanta@outlook.com>
2019-09-06 23:16:05 +0700

    Added exceptions to `declare global`

--- edge/typescript-interface-start-with-i.js
+++ edge/typescript-interface-start-with-i.js
@@ -9,6 +9,13 @@ module.exports = {
 		return {
 			TSInterfaceDeclaration: function (root) {
 				if (root.id && root.id.type === 'Identifier' && /^I[A-Z]/.test(root.id.name) === false) {
+					const nodes = context.getAncestors().reverse()
+					for (const node of nodes) {
+						if (node.type === 'TSModuleDeclaration' && node.id && node.id.type === 'Identifier' && node.id.name === 'global') {
+							return null
+						}
+					}
+
 					context.report({
 						node: root.id,
 						message: `Expected an interface name must start with a capital I.`,
@@ -22,6 +29,15 @@ module.exports = {
 			{
 				code: `
 					interface IName {}
+				`,
+				parser: '@typescript-eslint/parser',
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `
+					declare global {
+						interface Window {}
+					}
 				`,
 				parser: '@typescript-eslint/parser',
 				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
