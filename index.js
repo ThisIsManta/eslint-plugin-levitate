8dc113b3d68c9644e058ef78a394142b42137a35
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2019-10-13 22:30:39 +0700

    Removed unused code

--- index.js
+++ index.js
@@ -4,9 +4,5 @@ module.exports = {
 	rules: ruleList.reduce(function (hash, name) {
 		hash[name] = require('./edge/' + name)
 		return hash
-	}, {}),
-	rulesConfig: ruleList.reduce(function (hash, name) {
-		hash[name] = 'off'
-		return hash
 	}, {})
 }
