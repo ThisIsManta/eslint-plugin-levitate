577e772376694d5a9f1b42abe3fa6a98f493d49b
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2019-10-13 18:20:03 +0700

    Added rules from Taskworld frontend

---
+++ edge/comment.js
@@ -0,0 +1,170 @@
+'use strict'
+
+const _ = require('lodash')
+
+const FORMAL = /^\s(HACK|TODO):(\s\S|$)/
+const HACK = /^\s*(HACK|XXX)\W\s*/i
+const TODO = /^\s*TODO\W\s*/i
+const NOTE = /^\s*(Note\W)\s/i
+const URL = /^\s\w+:\/\/.+/
+
+module.exports = {
+  meta: {
+    docs: {
+      description: 'enforce writing consistent single-line comments',
+    },
+    fixable: 'code',
+  },
+  create: function(context) {
+    return {
+      Program: function(root) {
+        _.chain(root.comments)
+          .filter(node => node.type === 'Line')
+          .forEach((node, index, array) => {
+            if (FORMAL.test(node.value)) {
+              return null
+            }
+
+            if (HACK.test(node.value)) {
+              return context.report({
+                node,
+                message: `Expected the comment to be written as "HACK: ..."`,
+                fix: fixer =>
+                  fixer.replaceText(
+                    node,
+                    node.value.replace(HACK, '// HACK: ').trim()
+                  ),
+              })
+            }
+
+            if (TODO.test(node.value)) {
+              return context.report({
+                node,
+                message: `Expected the comment to be written as "TODO: ..."`,
+                fix: fixer =>
+                  fixer.replaceText(
+                    node,
+                    node.value.replace(TODO, '// TODO: ').trim()
+                  ),
+              })
+            }
+
+            if (URL.test(node.value)) {
+              return context.report({
+                node,
+                message: `Expected the comment to be written as "See ${node.value.trim()}"`,
+                fix: fixer => fixer.replaceText(node, '// See' + node.value),
+              })
+            }
+
+            if (NOTE.test(node.value)) {
+              return context.report({
+                node,
+                message: `Unexpected "${node.value.match(NOTE)[1].trim()}"`,
+                fix: fixer =>
+                  fixer.replaceText(
+                    node,
+                    node.value.replace(NOTE, '// ').trim()
+                  ),
+              })
+            }
+
+            // Skip if this is a part of consecutive single-line comments
+            if (
+              index > 0 &&
+              node.loc.start.line === array[index - 1].loc.start.line + 1
+            ) {
+              return null
+            }
+
+            // Skip if this is a single word or an ESLint instruction
+            const text = node.value.trim()
+            if (
+              text.includes(' ') === false ||
+              text.startsWith('eslint-disable')
+            ) {
+              return null
+            }
+
+            // Capitalize the first word
+            const firstChar = text.charAt(0)
+            if (firstChar !== firstChar.toUpperCase()) {
+              return context.report({
+                node,
+                message: `Expected the comment to start with a capital letter`,
+                fix: fixer =>
+                  fixer.replaceText(
+                    node,
+                    node.value.replace(
+                      new RegExp('^\\s*' + _.escapeRegExp(firstChar) + '\\s*'),
+                      '// ' + firstChar.toUpperCase()
+                    )
+                  ),
+              })
+            }
+          })
+          .value()
+      },
+    }
+  },
+  tests: {
+    valid: ['// HACK: lorem', '// TODO: lorem', '// Lorem'],
+    invalid: [
+      {
+        code: '// Hack lorem',
+        errors: [
+          {
+            message: 'Expected the comment to be written as "HACK: ..."',
+          },
+        ],
+        output: '// HACK: lorem',
+      },
+      {
+        code: '// XXX: lorem',
+        errors: [
+          {
+            message: 'Expected the comment to be written as "HACK: ..."',
+          },
+        ],
+        output: '// HACK: lorem',
+      },
+      {
+        code: '// Todo: lorem',
+        errors: [
+          {
+            message: 'Expected the comment to be written as "TODO: ..."',
+          },
+        ],
+        output: '// TODO: lorem',
+      },
+      {
+        code: '// http://www.example.com/xxx',
+        errors: [
+          {
+            message:
+              'Expected the comment to be written as "See http://www.example.com/xxx"',
+          },
+        ],
+        output: '// See http://www.example.com/xxx',
+      },
+      {
+        code: '// Note: lorem',
+        errors: [
+          {
+            message: 'Unexpected "Note:"',
+          },
+        ],
+        output: '// lorem',
+      },
+      {
+        code: ['// lorem ipsum', '// dolor sit'].join('\n'),
+        errors: [
+          {
+            message: 'Expected the comment to start with a capital letter',
+          },
+        ],
+        output: ['// Lorem ipsum', '// dolor sit'].join('\n'),
+      },
+    ],
+  },
+}
---
+++ edge/test-case-title.js
@@ -0,0 +1,116 @@
+'use strict'
+
+const _ = require('lodash')
+
+module.exports = {
+  meta: {
+    docs: {
+      description: 'enforce writing consistent test case titles',
+    },
+    messages: {
+      naming:
+        'Expected test case titles to start with "(does not) return/render/call/fetch/set/throw(s) ... (, given ...)" only',
+    },
+  },
+  create: function(context) {
+    // Skip non-test files
+    if (/\.(test|spec)\.(j|t)sx?$/.test(context.getFilename()) === false) {
+      return {}
+    }
+
+    return {
+      ExpressionStatement: function(root) {
+        if (
+          /^(it|test)$/.test(_.get(root, 'expression.callee.name')) === false
+        ) {
+          return null
+        }
+
+        const node = _.get(root, 'expression.arguments.0')
+        if (
+          node &&
+          _.isString(node.value) &&
+          /^(returns|renders|calls|fetches|sets|throws|does not (return|render|call|fetch|set|throw) )/.test(
+            node.value
+          ) === false
+        ) {
+          context.report({
+            node,
+            messageId: 'naming',
+          })
+        }
+      },
+    }
+  },
+  tests: {
+    valid: [
+      {
+        code: 'it("returns something", function() {})',
+        filename: 'file.test.ts',
+        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+      },
+      {
+        code: 'it("renders something", function() {})',
+        filename: 'file.test.ts',
+        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+      },
+      {
+        code: 'it("calls something", function() {})',
+        filename: 'file.test.ts',
+        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+      },
+      {
+        code: 'it("fetches something", function() {})',
+        filename: 'file.test.ts',
+        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+      },
+      {
+        code: 'it("sets something", function() {})',
+        filename: 'file.test.ts',
+        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+      },
+      {
+        code: 'it("throws an error", function() {})',
+        filename: 'file.test.ts',
+        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+      },
+      {
+        code: 'it("does not return something", function() {})',
+        filename: 'file.js',
+        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+      },
+    ],
+    invalid: [
+      {
+        code: 'it("does not renders something", function() {})',
+        filename: 'file.test.ts',
+        errors: [
+          {
+            messageId: 'naming',
+          },
+        ],
+        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+      },
+      {
+        code: 'it("displays something", function() {})',
+        filename: 'file.test.ts',
+        errors: [
+          {
+            messageId: 'naming',
+          },
+        ],
+        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+      },
+      {
+        code: 'it("should do something", function() {})',
+        filename: 'file.test.ts',
+        errors: [
+          {
+            messageId: 'naming',
+          },
+        ],
+        parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+      },
+    ],
+  },
+}
