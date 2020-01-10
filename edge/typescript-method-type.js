db764516e8e3df53092bd40fa4024ff4118ef99c
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2020-01-10 15:43:00 +0700

    Added new rule: typescript-method-type

---
+++ edge/typescript-method-type.js
@@ -0,0 +1,103 @@
+const _ = require('lodash')
+
+module.exports = {
+  meta: {
+    docs: {
+      description: 'enforce writing function types using arrow notation',
+      category: 'Stylistic Issues',
+    },
+    fixable: 'code',
+  },
+  create: function(context) {
+    return {
+      TSMethodSignature: function(root) {
+        const sourceCode = context.getSourceCode()
+        context.report({
+          node: root,
+          message: 'Expected to be using arrow notation',
+          fix: fixer =>
+            fixer.replaceText(
+              root,
+              sourceCode.getText(root.key) +
+                (root.optional ? '?' : '') +
+                ': ' +
+                sourceCode.getText(root.typeParameters) +
+                '(' +
+                _.map(root.params, node => sourceCode.getText(node)).join(
+                  ', '
+                ) +
+                ') => ' +
+                (root.returnType
+                  ? sourceCode.getText(root.returnType).replace(/^:\s*/, '')
+                  : 'void')
+            ),
+        })
+      },
+    }
+  },
+  tests: {
+    valid: [
+      {
+        code: `
+        interface X {
+          onClick: <T>(a, b, c) => void
+        }
+        `,
+        parser: require.resolve('@typescript-eslint/parser'),
+        parserOptions: {
+          ecmaVersion: 6,
+          sourceType: 'module',
+          ecmaFeatures: { jsx: true },
+        },
+      },
+    ],
+    invalid: [
+      {
+        code: `
+        interface X {
+          onClick<T>(a, b, c): void
+        }
+        `,
+        parser: require.resolve('@typescript-eslint/parser'),
+        parserOptions: {
+          ecmaVersion: 6,
+          sourceType: 'module',
+          ecmaFeatures: { jsx: true },
+        },
+        errors: [
+          {
+            message: 'Expected to be using arrow notation',
+            output: `
+            interface X {
+              onClick: <T>(a, b, c) => void
+            }
+            `,
+          },
+        ],
+      },
+      {
+        code: `
+        interface X {
+          onClose?<T>()
+        }
+        `,
+        parser: require.resolve('@typescript-eslint/parser'),
+        parserOptions: {
+          ecmaVersion: 6,
+          sourceType: 'module',
+          ecmaFeatures: { jsx: true },
+        },
+        errors: [
+          {
+            message: 'Expected to be using arrow notation',
+            output: `
+            interface X {
+              onClose?: () => void
+            }
+            `,
+          },
+        ],
+      },
+    ],
+  },
+}
