3f8cbd2975c3d1ba718eaebd91c214fdac1c4ee2
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2020-02-15 18:03:39 +0700

    Add new rule: typescript-interface-name

---
+++ edge/typescript-interface-name.js
@@ -0,0 +1,53 @@
+const _ = require('lodash')
+
+module.exports = {
+	meta: {
+		docs: {
+			description: 'enforce naming user-defined interfaces starting with "I"',
+			category: 'Stylistic Issues',
+		},
+	},
+	create: function (context) {
+		return {
+			TSInterfaceDeclaration: function (root) {
+				if (/^I[A-Z]/.test(root.id.name)) {
+					return
+				}
+
+				for (const node of context.getAncestors()) {
+					if (node.type === 'TSModuleDeclaration') {
+						return
+					}
+				}
+
+				context.report({
+					node: root.id,
+					message: `Expected the interface to start with "I"`,
+				})
+			}
+		}
+	},
+	test: {
+		valid: [
+			{
+				code: `interface IWindow {}`,
+				parser: require.resolve('@typescript-eslint/parser'),
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `declare global { interface Window {} }`,
+				parser: require.resolve('@typescript-eslint/parser'),
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+		],
+		invalid: [
+			{
+				code: `interface Window {}`,
+				options: ['PascalCase'],
+				parser: require.resolve('@typescript-eslint/parser'),
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+				errors: [{ message: 'Expected the interface to start with "I"' }],
+			},
+		]
+	}
+}
