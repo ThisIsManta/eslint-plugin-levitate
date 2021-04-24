6deeaf90d163f8716ddf2dd41de5916c1e8d5a19
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2021-04-25 03:19:47 +0700

    Add new-line-before-chain

---
+++ edge/new-line-before-chain.js
@@ -0,0 +1,79 @@
+const _ = require('lodash')
+
+module.exports = {
+	meta: {
+		docs: {
+			description: 'enforce having a new line per chaining method',
+			category: 'Stylistic Issues',
+		},
+		fixable: 'whitespace',
+		messages: {
+			error: 'Expected a new line before each chaining method.',
+		}
+	},
+	create: function (context) {
+		return {
+			'CallExpression:exit': function (root) {
+				if (root.parent.type !== 'MemberExpression') {
+					return
+				}
+				
+				if (root.loc.end.line === root.parent.property.loc.start.line) {
+					const sourceCode = context.getSourceCode()
+					const dot = sourceCode.getTokenBefore(root.parent.property)
+					context.report({
+						loc: dot.loc,
+						messageId: 'error',
+						fix: (fixer) => fixer.insertTextBefore(dot, '\n')
+					})	
+				}
+			}
+		}
+	},
+	tests: {
+		valid: [
+			{
+				code: `
+					_.chain()
+					  .map()
+						.prop.reduce()
+						.value()
+				`,
+			},
+		],
+		invalid: [
+			{
+				code: `
+					_.chain().map().prop.reduce().value()
+				`,
+				output: `
+					_.chain()
+.map()
+.prop.reduce()
+.value()
+				`,
+				errors: [
+					{ messageId: 'error', line: 2 },
+					{ messageId: 'error', line: 2 },
+					{ messageId: 'error', line: 2 },
+				],
+			},
+			{
+				code: `
+					_.chain()
+						.map().prop.reduce().value()
+				`,
+				output: `
+					_.chain()
+						.map()
+.prop.reduce()
+.value()
+				`,
+				errors: [
+					{ messageId: 'error', line: 3 },
+					{ messageId: 'error', line: 3 },
+				],
+			},
+		]
+	}
+}
--- tests.js
+++ tests.js
@@ -1,15 +1,17 @@
 const TestRunner = require('eslint').RuleTester
 const ruleList = require('./rules')
 
-ruleList.forEach(function (name) {
-	const rule = require('./edge/' + name)
-	if (rule.tests) {
-		console.log('✅ ' + name)
-		new TestRunner({
-			parserOptions: { ecmaVersion: 6, sourceType: 'module' }
-		}).run(name, rule, rule.tests)
-	} else {
-		console.log('⛔ ' + name)
-	}
-})
+ruleList
+	.filter(name => process.argv.length > 2 ? process.argv.includes(name) : true)
+	.forEach(function (name) {
+		const rule = require('./edge/' + name)
+		if (rule.tests) {
+			console.log('✅ ' + name)
+			new TestRunner({
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' }
+			}).run(name, rule, rule.tests)
+		} else {
+			console.log('⛔ ' + name)
+		}
+	})
 console.log('Done')
