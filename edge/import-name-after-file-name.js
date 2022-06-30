b148e9bcaccdb82dd7eca440a1564ab1ede48c73
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2022-06-30 14:11:43 +0700

    Amend import-name-after-file-name

--- edge/import-name-after-file-name.js
+++ edge/import-name-after-file-name.js
@@ -1,8 +1,10 @@
 const fp = require('path')
 const _ = require('lodash')
 const { getImportFullPath } = require('./import-path-from-closest-index')
+const { singular } = require('pluralize')
 
-const ALPHANUMERIC = /[A-Za-z0-9]/
+const validIdentifierPattern = /^[A-Z_$][0-9A-Z_$]*$/i
+const acronym = /^(https?|xhr|html|xml|yml|url|pwa|io|ui|api|sdk)$/i
 
 module.exports = {
 	meta: {
@@ -12,7 +14,29 @@ module.exports = {
 		},
 	},
 	create: function (context) {
+		const stripper = new RegExp(context.options[0] || '')
+
+		const takenNames = new Set()
+
+		const reports = []
+
 		return {
+			Program: function (root) {
+				_.chain(root.body)
+					.flatMap(node =>
+						context.getDeclaredVariables(
+							(node.type === 'ExportNamedDeclaration' || node.type === 'ExportDefaultDeclaration')
+								? node.declaration
+								: node
+						)
+					)
+					.map(variable => variable.name)
+					.compact()
+					.value()
+					.forEach(name => {
+						takenNames.add(name)
+					})
+			},
 			ImportDeclaration: function (root) {
 				if (!root.specifiers || root.specifiers.length === 0) {
 					return
@@ -28,26 +52,64 @@ module.exports = {
 					return
 				}
 
-				const fullPath = getImportFullPath(context.getFilename(), workPath) || workPath
-				const fileNameWithoutExtension = fp.basename(fullPath, fp.extname(fullPath))
-				const directoryName = fp.basename(fp.dirname(fullPath))
-				const expectedName = fileNameWithoutExtension === 'index'
-					? _.get(directoryName.match(ALPHANUMERIC), '0', '').toUpperCase() + _.camelCase(directoryName).substring(1)
-					: _.get(fileNameWithoutExtension.match(ALPHANUMERIC), '0', '') + _.camelCase(fileNameWithoutExtension.replace(/\..+/, '')).substring(1)
+				const absoluteFilePath = getImportFullPath(context.getFilename(), workPath) || workPath
+				const strippedFileName = fp.basename(absoluteFilePath).replace(stripper, '').replace(/\..+/, '')
+				const strippedDirectoryName = fp.basename(fp.dirname(absoluteFilePath)).replace(stripper, '')
 
-				// Check for another duplicate identifier
-				const otherImports = _.last(context.getAncestors()).body.filter(node => node.type === 'ImportDeclaration' && node !== root)
-				if (otherImports.some(node => node.specifiers && node.specifiers.some(spec => spec.local.name === expectedName))) {
+				const expectedNames = _.uniq(_.compact((() => {
+					if (strippedFileName === 'index') {
+						const _DirectoryName = upperAllIfAcronym(upperEach(noLeadingDigits(strippedDirectoryName)))
+						return [_DirectoryName]
+					}
+
+					const _fileName = validIdentifierPattern.test(strippedFileName)
+						? strippedFileName
+						: upperSecond(noLeadingDigits(strippedFileName))
+
+					if (workNode.type === 'ImportNamespaceSpecifier') {
+						const _FileName = _.upperFirst(_fileName)
+						const _CombinedName = upperEach(singular(strippedDirectoryName) + '/' + upperAllIfAcronym(strippedFileName))
+
+						return [_FileName, _CombinedName]
+					}
+
+					return [_fileName]
+				})()))
+
+				// Do nothing if one of the expected names is conflicting with the existing names in the same file
+				if (expectedNames.some(name => takenNames.has(name))) {
 					return
 				}
 
-				if (workNode.local.name !== expectedName) {
-					context.report({
+				if (expectedNames.every(expectedName => expectedName !== workNode.local.name)) {
+					reports.push({
 						node: workNode.local,
-						message: `Expected "${workNode.local.name}" to be "${expectedName}".`,
+						expectedNames,
 					})
 				}
-			}
+			},
+			'Program:exit': function () {
+				const expectedNameToReportedNodes = {}
+				for (const { node, expectedNames } of reports) {
+					for (const name of expectedNames) {
+						const nodes = expectedNameToReportedNodes[name] || []
+						nodes.push(node)
+						expectedNameToReportedNodes[name] = nodes
+					}
+				}
+
+				for (const { node, expectedNames } of reports) {
+					const nonConflictingExpectedNames = expectedNames.filter(name => expectedNameToReportedNodes[name].length === 1)
+					if (nonConflictingExpectedNames.length === 0) {
+						return
+					}
+
+					context.report({
+						node,
+						message: `Expected "${node.name}" to be "${nonConflictingExpectedNames.join('" or "')}"`,
+					})
+				}
+			},
 		}
 	},
 	tests: {
@@ -57,7 +119,7 @@ module.exports = {
 				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
 			},
 			{
-				code: `import * as aaa from './aaa'`,
+				code: `import * as Aaa from './aaa'`,
 				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
 			},
 			{
@@ -80,23 +142,85 @@ module.exports = {
 				code: `import XxxYyy from '../xxx-yyy/index'`,
 				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
 			},
+			{
+				code: `import createIO from '../createIO'`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `import operationQueue from '../operation-queue'`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `import * as FlashMessage from '../tw-flash-message'`,
+				options: ['^tw-'],
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `import * as MessageAPI from '../messages/api'`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `import * as MessageURLs from '../messages/urls'`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `import bbb from './aaa'; const aaa = 1`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `import bbb from './aaa'; export function aaa() {}`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `import bbb from './aaa'; export default function aaa() {}`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `import aaa from './aaa/reducer'; import bbb from './aaa/reducer'`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
 		],
 		invalid: [
 			{
 				code: `import XXX from './aaa'`,
 				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
-				errors: [{ message: 'Expected "XXX" to be "aaa".' }],
+				errors: [{ message: 'Expected "XXX" to be "aaa"' }],
 			},
 			{
 				code: `import * as XXX from './aaa'`,
 				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
-				errors: [{ message: 'Expected "XXX" to be "aaa".' }],
+				errors: [{ message: 'Expected "XXX" to be "Aaa"' }],
 			},
 			{
 				code: `import * as XXX from '../xxx-yyy/index'`,
 				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
-				errors: [{ message: 'Expected "XXX" to be "XxxYyy".' }],
+				errors: [{ message: 'Expected "XXX" to be "XxxYyy"' }],
 			},
 		]
 	}
+}
+
+function upperEach(text) {
+	return _.words(text).map(word => _.upperFirst(word)).join('')
+}
+
+/**
+ * Returns capitalized first character of each word, except the first word, while keeping everything else untouched
+ */
+function upperSecond(text) {
+	return _.words(text).map((word, rank) => rank === 0 ? _.toLower(word) : _.upperFirst(word)).join('')
+}
+
+function upperAllIfAcronym(text) {
+	const single = singular(text)
+	if (acronym.test(single)) {
+		return _.toUpper(single) + text.substring(single.length)
+	}
+
+	return text
+}
+
+const digitOnly = /^\d+$/
+function noLeadingDigits(text) {
+	return _.dropWhile(_.words(text), word => digitOnly.test(word)).join('-')
 }
