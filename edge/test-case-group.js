7fd078373e54f0e78bc1661831ac3334ef37f360
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2023-03-08 16:41:33 +0700

    fix: false positive on object describe block name

--- edge/test-case-group.js
+++ edge/test-case-group.js
@@ -8,49 +8,130 @@ module.exports = {
 		docs: {
 			description: 'enforce using a function reference as a test case description',
 		},
-		messages: {
-			direct: 'Expected the description to be the direct reference of the under-testing function by removing the string quotes',
-		},
 		fixable: 'code',
 	},
 	create: function (context) {
 		return {
 			ExpressionStatement: function (root) {
-				const functionCall = _.get(root, 'expression.callee.name')
-				if (functionCall !== 'describe') {
+				const describeBlockFound = _.isMatch(root, { expression: { type: 'CallExpression', callee: { type: 'Identifier', name: 'describe' } } })
+				if (!describeBlockFound) {
 					return
 				}
 
-				const titleNode = _.get(root, 'expression.arguments.0')
-				const titleText = getText(titleNode)
-				if (_.isString(titleText) === false) {
+				const describeNameNode = _.get(root, 'expression.arguments.0')
+				if (!describeNameNode) {
 					return
 				}
 
-				const scope = context.getScope()
-				const matchingNode = scope.variables.find(node => node.name === titleText)
-				if (!matchingNode) {
-					return
+				const describeName = getText(describeNameNode)
+				if (typeof describeName === 'string') {
+					if (describeName.trim().length === 0) {
+						return
+					}
+
+					const [objectName, ...propertyAccessorName] = describeName.split('.')
+					const testingNodes = _.compact([
+						propertyAccessorName.reduce((object, name) => {
+							return { type: 'MemberExpression', object, property: { type: 'Identifier', name } }
+						}, { type: 'Identifier', name: objectName }),
+						propertyAccessorName.length === 0 && { type: 'MemberExpression', property: { type: 'Identifier', name: describeName } },
+					])
+
+					if (!lookForObject(testingNodes) && !lookForFunctionCall(testingNodes)) {
+						context.report({
+							node: describeNameNode,
+							message: 'Expected the describe block name to match an object name, a namespace or a function name',
+						})
+					}
+
+				} else if (describeNameNode.type === 'Identifier' || describeNameNode.type === 'MemberExpression') {
+					const testingNodes = [copyNode(describeNameNode)]
+					if (!lookForFunctionCall(testingNodes)) {
+						context.report({
+							node: describeNameNode,
+							message: 'Expected the identifier to be a function being called somewhere in the describe block',
+						})
+					}
 				}
 
-				if (matchingNode.references.some(ref => _.get(ref, 'identifier.parent.type') === 'CallExpression')) {
-					return context.report({
-						node: titleNode,
-						messageId: 'direct',
-						fix: fixer => fixer.replaceText(titleNode, titleText)
-					})
+				function lookForObject(testingNodes) {
+					const { variables } = context.getScope()
+					return variables.some(variable =>
+						variable.defs.some(({ name }) => {
+							return testingNodes.filter(({ type }) => type === 'Identifier').some(testingNode => _.isMatch(name, testingNode))
+						}) ||
+						variable.references.some(({ identifier }) => {
+							return testingNodes.filter(({ type }) => type === 'MemberExpression').some(testingNode => findParentNode(identifier, testingNode))
+						})
+					)
+				}
+
+				function lookForFunctionCall(testingNodes) {
+					const { variables } = context.getScope()
+					return variables.some(variable => variable.references.some(({ identifier }) => {
+						const functionCallNode = findFunctionCallNode(identifier)
+						if (functionCallNode) {
+							return testingNodes.some(testingNode => _.isMatch(functionCallNode, { callee: testingNode }))
+						}
+
+						return false
+					}))
 				}
 			},
 		}
 	},
 	tests: {
-		valid: [
+		valid: [,
 			{
 				code: `
-				import { doSomething } from 'xxx'
-				describe(doSomething, function() {
-					it("does something", function() {
-						doSomething()
+				import { func } from 'xxx'
+				describe('func', function() {
+					it('xxx', function() {
+						func()
+					})
+				})
+				`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `
+				import { func } from 'xxx'
+				describe(func, function() {
+					it('xxx', function() {
+						func()
+					})
+				})
+				`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `
+				import { object } from 'xxx'
+				describe('object', function() {
+					it('xxx', function() {
+						expect(object).toEqual({})
+					})
+				})
+				`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `
+				import * as Namespace from 'xxx'
+				describe('Namespace.property.func', function() {
+					it('xxx', function() {
+						expect(Namespace.property.func())
+					})
+				})
+				`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+			},
+			{
+				code: `
+				import * as Namespace from 'xxx'
+				describe(Namespace.func, function() {
+					it('xxx', function() {
+						expect(Namespace.func())
 					})
 				})
 				`,
@@ -61,23 +142,76 @@ module.exports = {
 			{
 				code: `
 				import { doSomething } from 'xxx'
-				describe("doSomething", function() {
-					it("does something", function() {
+				describe('something else', function() {
+					it('xxx', function() {
 						doSomething()
 					})
 				})
 				`,
 				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
-				errors: [{ messageId: 'direct' }],
-				output: `
+				errors: [{ message: 'Expected the describe block name to match an object name, a namespace or a function name' }],
+			},
+			{
+				code: `
 				import { doSomething } from 'xxx'
 				describe(doSomething, function() {
-					it("does something", function() {
-						doSomething()
+				})
+				`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+				errors: [{ message: 'Expected the identifier to be a function being called somewhere in the describe block' }],
+			},
+			{
+				code: `
+				import * as Namespace from 'xxx'
+				describe(Namespace.func, function() {
+					it('xxx', function() {
+						expect(Namespace.func)
 					})
 				})
-				`
+				`,
+				parserOptions: { ecmaVersion: 6, sourceType: 'module' },
+				errors: [{ message: 'Expected the identifier to be a function being called somewhere in the describe block' }],
 			},
 		],
 	},
+}
+
+function copyNode(node) {
+	if (node.type === 'MemberExpression') {
+		return { type: 'MemberExpression', object: copyNode(node.object), property: copyNode(node.property) }
+	}
+
+	if (node.type === 'Identifier') {
+		return _.pick(node, 'type', 'name')
+	}
+
+	return _.pick(node, 'type')
+}
+
+function findParentNode(node, findingNode) {
+	if (!node) {
+		return null
+	}
+
+	if (_.isMatch(node, findingNode)) {
+		return node
+	}
+
+	return findParentNode(node.parent, findingNode)
+}
+
+function findFunctionCallNode(node) {
+	if (!node || !node.parent) {
+		return null
+	}
+
+	if (node.parent.type === 'CallExpression' && node.parent.callee === node) {
+		return node.parent
+	}
+
+	if (node.parent.type === 'MemberExpression' && node.parent.object === node) {
+		return findFunctionCallNode(node.parent)
+	}
+
+	return null
 }
