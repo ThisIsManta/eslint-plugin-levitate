c83e640476a64f14976c54593fced1b2c82b763e
Anantachai Saothong (Manta) <anantachai.s@taskworld.com>
2023-04-26 17:50:17 +0700

    chore: allow running exclusive test cases

--- tests.js
+++ tests.js
@@ -1,16 +1,31 @@
 const TestRunner = require('eslint').RuleTester
-const detectIndent = require('detect-indent')
 const chalk = require('chalk')
+const _ = require('lodash')
+const { parseArguments } = require('@thisismanta/pessimist')
 
 const tester = new TestRunner({
 	parserOptions: { ecmaVersion: 6, sourceType: 'module' }
 })
 
-const narrowingWords = process.argv.slice(2).filter(word => !word.startsWith('-'))
+const { debug: debugging, ...options } = parseArguments(
+	process.argv.slice(2),
+	{
+		debug: !!process.env.DEBUG,
+	},
+	{
+		aliases: [['d', 'debug']]
+	}
+)
+
+const exclusiveTestCases = []
+global.only = function only(testCase) {
+	exclusiveTestCases.push(testCase)
+	return testCase
+}
+
+const narrowingWords = Array.from(options)
 const ruleNames = require('./rules')
 	.filter(name => narrowingWords.length > 0 ? narrowingWords.some(text => name.includes(text)) : true)
-
-const debugging = !!process.env.DEBUG || process.argv.includes('--debug') || process.argv.includes('-d')
 
 if (ruleNames.length === 0) {
 	console.error(chalk.red('Could not find any rules to test.'))
@@ -20,31 +35,29 @@ if (ruleNames.length === 0) {
 for (const name of ruleNames) {
 	const rule = require('./edge/' + name)
 	if (!rule.tests) {
-		console.log('🚧 ' + name)
+		console.log('🟡 ' + name)
 		continue
 	}
 
 	if (debugging) {
-		console.log('🟡 ' + name)
+		console.log('⬜ ' + name)
 	}
 
+	let skipped = false
 	for (const type of ['valid', 'invalid']) {
 		for (const test of rule.tests[type]) {
 			if (!test || !test.code) {
 				continue
 			}
 
-			const { indent } = detectIndent(test.code)
-			const code = test.code.split('\n')
-				.filter((line, rank, list) =>
-					(rank === 0 || rank === list.length - 1) ? line.trim().length > 0 : true
-				)
-				.map(line => line.replace(new RegExp('^' + indent), ''))
-				.join('\n')
+			if (exclusiveTestCases.length > 0 && !exclusiveTestCases.includes(test)) {
+				skipped = true
+				continue
+			}
 
 			if (debugging) {
 				console.log()
-				console.log(chalk.bgWhiteBright(code))
+				console.log(offset(getPrettyCode(test.code), chalk.bgWhiteBright))
 				console.log()
 			}
 
@@ -55,19 +68,43 @@ for (const name of ruleNames) {
 				console.log('🔴 ' + chalk.bold(name))
 
 				console.log()
-				console.log(chalk.bgRed(code))
+				console.log(offset(getPrettyCode(test.code), chalk.bgRed))
 				console.log()
 
-				console.error(chalk.red(error.message))
+				console.error(offset(error.message, chalk.red))
+				if (debugging && error.stack) {
+					console.error(offset(error.stack, chalk.red))
+				}
+
 				process.exit(1)
 			}
 		}
 	}
 
-	if (narrowingWords.length > 0) {
-		console.log('🟢 ' + name)
-	}
+	console.log((skipped ? '🟡' : '✅') + ' ' + name)
 }
 
 console.log()
 console.log(`Done testing ${ruleNames.length} rules.`)
+
+function offset(text, decorateLine = line => line) {
+	return text.split('\n').map(line => '   ' + decorateLine(line)).join('\n')
+}
+
+function getPrettyCode(text) {
+	const trimmedCode = text.split('\n').filter((line, rank, list) =>
+		(rank === 0 || rank === list.length - 1) ? line.trim().length > 0 : true
+	)
+
+	const indent = _.minBy(
+		trimmedCode
+			.filter(line => line.trim().length > 0)
+			.map(line => (line.match(/^(\t|\s)+/) || [''])[0]),
+		indent => indent.length
+	) || ''
+
+	return trimmedCode.map(line => line
+		.replace(new RegExp('^' + indent), '')
+		.replace(/^\t+/, tabs => '  '.repeat(tabs.length))
+	).join('\n')
+}
